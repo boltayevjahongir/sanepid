0799489212c5ed05b6146aa0bece68fe15480b80
boltayev.jahongir <boltayev.jahongir@gmail.com>
2022-10-10 09:08:18 +0500

    add first

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * go to top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+});
